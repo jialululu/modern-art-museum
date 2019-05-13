04706415c41c511ae6ada31264cb4953bdae9b3c
Yichen Jia <yichenj@mit.edu>
2019-05-13 02:45:28 -0400

    add rec tooltip

--- utility.py
+++ utility.py
@@ -1,6 +1,6 @@
 import json
 
-with open('datasets/compress_color_artwork.json') as json_file:
+with open('datasets/moma_artist_final.json') as json_file:
     data = json.load(json_file)
 
 print(data[0].keys())
@@ -11,13 +11,13 @@ print(len(data))
 #
 # print(data2[0].keys())
 # print(len(data2))
-for d in data:
-    acum = 0
-    color = d['Domain color']
-    for c in color:
-        c.append(acum)
-        acum += c[1]
-
-# print(data)
-with open('datasets/compress_color_artwork_final.json','w') as fp:
-    json.dump(data,fp)
+# for d in data:
+#     acum = 0
+#     color = d['Domain color']
+#     for c in color:
+#         c.append(acum)
+#         acum += c[1]
+#
+# # print(data)
+# with open('datasets/compress_color_artwork_final.json','w') as fp:
+#     json.dump(data,fp)
